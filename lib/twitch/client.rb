0a536f605fe5bc589510bdba0767eb95c1ee3436
Ameya <46313021+absolut-zero@users.noreply.github.com>
2023-09-06 11:18:05 +1000

    Update parameter names

--- lib/twitch/client.rb
+++ lib/twitch/client.rb
@@ -393,8 +393,8 @@ module Twitch
     end
 
     def follow_status(user_id, channel_id, options = {})
-      options[:from_id] = user_id
-      options[:to_id] = channel_id
+      options[:user_id] = user_id
+      options[:broadcaster_id] = channel_id
       query = build_query_string(options)
       path = "/channels/followed"
       url = @base_url + path + query
